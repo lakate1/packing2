91cbd109a7e1a96c648c973329ba02619913ee11
lakate1 <31014745+lakate1@users.noreply.github.com>
2018-06-04 19:26:45 -0700

    commit

--- models/packing.ts
+++ models/packing.ts
@@ -1,9 +1,9 @@
 import mongoose = require('mongoose');
 
 let PackingSchema = new mongoose.Schema({
-    destination : {type:String, lowercase: true, unique:true},
+    destination : {type:String, lowercase: true},
     season : String,
-    items: [],
+    items: {type: Array,},
     userid: {type: String, required: true}
 });
   export default mongoose.model('Packing', PackingSchema);
--- routes/users.ts
+++ routes/users.ts
@@ -5,10 +5,12 @@ import jwt = require('jsonwebtoken');
 import User from '../models/user';
 let router = express.Router();
 
+// POST - /Register this needs to be changed to save the user role
 router.post('/Register', (req, res, next) => {
   let user:any = new User();
   user.username = req.body.username;
   user.setPassword(req.body.password);
+  user.role(req.body.userrole);
 
   user.save(function(err, newUser){
     if(err){
@@ -19,17 +21,8 @@ router.post('/Register', (req, res, next) => {
     res.status(500);
   });
 });
-//Put in verify Token 
+
 router.post('/Login/local', (req, res, next) => {
-  // jwt.verify(req.token,"secretkey", (err, authData) => {
-  //   if(err) {
-  //     res.sendStatus(403);
-  //   } else {
-      
-  //   }
-
-  // })
-
   if(!req.body.username || !req.body.password){
     res.status(400).json({message:"Please fill in all fields."});
   }
@@ -45,32 +38,10 @@ router.post('/Login/local', (req, res, next) => {
 });
 
 
-
+//this needs to be changed to get all lists who's userId matches the Id of the user logged in 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/list', function(req, res, next) {
   res.send('respond with a resource');
 });
-// //Format of Token
-// //Authorization: Bearer <access Token
 
-// //Verify Token
-// function verifyToken(req,res,next) {
-// //Get auth header value
-// let bearerHeader = req.headers["authorization"];
-// //Check if bearer is undefined
-// if(typeof bearerHeader !== "undefined") {
-//   //Split at the space
-//   let bearer = bearerHeader.split(" ");
-//   //Get token from array
-//   let bearerToken = bearer[1];
-//   //Set the Token
-//   req.token = bearerToken;
-//   //Next middleware
-//   next();
-
-// } else {
-//   //Forbidden
-//   res.sendstatus(403);
-// }
-// }
 export default router;
